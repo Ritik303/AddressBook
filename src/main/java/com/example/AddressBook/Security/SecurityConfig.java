f6f2b2e06cb6f82284144e4ef0e44c7f993331fb
Ritik303 <rtk3032002@gmail.com>
2025-03-18 22:04:23 +0530

    Rabbit MQ

--- src/main/java/com/example/AddressBook/Security/SecurityConfig.java
+++ src/main/java/com/example/AddressBook/Security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.AddressBook.Security;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -17,7 +16,7 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(auth -> auth
                         //  Permit access to authentication APIs
-                        .requestMatchers(HttpMethod.POST, "/api/auth/register", "/api/auth/login","/api/auth/login-with-token").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/auth/register", "/api/auth/login","/api/auth/login-with-token", "/api/auth/send-message").permitAll()
                         .requestMatchers(HttpMethod.PUT, "/api/auth/forgotPassword/**", "/api/auth/resetPassword/**").permitAll()
 
                         //  Permit access to AddressBook APIs
